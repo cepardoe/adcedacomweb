1825975cdf02d3e33e6fe3d5209c207e20d66f8a
David Navarro Rolon <davidnavarrorolon@MacBook-Pro-de-David.local>
2020-02-05 18:02:32 -0500

    final test

--- mail.php
+++ mail.php
@@ -1,14 +1,14 @@
 <?php
  
-if(isset($_POST['send'])) {
-    
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $mail = $_POST['mail'];
-    $msg = $nombre . "" . $apellido;
+    if(isset($_POST['send'])) {
+        
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        $mail = $_POST['mail'];
+        $msg = $nombre . "" . $apellido;
 
-    mail($mail, "Prueba mail", $msg);
+        mail($mail, "Prueba mail", $msg);
 
-    echo 'hola';
-}
+        echo 'Enviado';
+    }
 ?>  
