fe710b98b8a9851f0174c98721780aa74f992f42
David Navarro <davidnavarrorolon@gmail.com>
2020-02-12 14:06:01 -0500

    avances

--- mail.php
+++ mail.php
@@ -1,5 +1,8 @@
 <?php
 
+// conexion db
+$mysqli = new mysqli("database-1.cmlpaj0d1yqv.us-east-2.rds.amazonaws.com", "admin", "Adceda2020", "web_page");
+
 if(isset($_POST['sendContacto'])) {
 	$nombre = $_POST['nombre'];
 	$apellido = $_POST['apellido'];
@@ -8,13 +11,19 @@ if(isset($_POST['sendContacto'])) {
 	$empleados = $_POST['cantidadEmpleados'];
 	$producto = $_POST['productoRequerido'];
 	$msg = $_POST['mensaje'];
-	$tabla = $nombre .' '. $apellido .' '. $mail .' '. $tel .' '. $empleados .' '. $producto .' '. $msg;   
-	mail('davidnavarrorolon@gmail.com', "Solicitud Página Web", $tabla); 
+	$tabla = $nombre .' - '. $apellido .' - '. $mail .' - '. $tel .' - '. $empleados .' - '. $producto .' - '. $msg;   
+	//envío correo
+	mail('mercadeoyventas@adceda.com.co', "Solicitud de Software", $tabla); 
+	// insercion db
+	$insert = $mysqli->query("INSERT INTO clientes (nombre, apellido, mail, telefono, empleados, producto, mensaje) VALUES ('$nombre', '$apellido', '$mail', '$tel', '$empleados', '$producto', '$msg')");
 }
 
 if(isset($_POST['mailSus'])) {
 	$mailsus = $_POST['suscripcion'];
-	mail('davidnavarrorolon@gmail.com', "Solicitud Suscripción", $mailsus);
+	// envío correo
+	mail('mercadeoyventas@adceda.com.co', "Solicitud de Suscripción", $mailsus);
+	// insercion db
+	$insert = $mysqli->query("INSERT INTO suscriptores (correo) VALUES ('$mailsus')");
 }
 
 ?>  
